8294696b732e51ec2595129163ead5c5f9a12505
Hareij <graslinmarcel8@gmail.com>
2025-08-21 20:18:17 +0200

    Add files via upload

---
+++ number_systems.cpp
@@ -0,0 +1,16 @@
+/*
+#include<iostream>
+using namespace std;
+int main() {
+    int number1 = 15; // Decimal
+    int number2 = 017; //octal
+    int number3 = 0xF; // Hexadecimal
+    int number4 = 0b1111; // Binary (C++14 and later
+
+    cout << "Number1 : " << number1 << endl;
+    cout << "Number2 : " << number2 << endl;
+    cout << "Number3 : " << number3 << endl;
+    cout << "Number4 : " << number4 << endl;
+    return 0;
+}
+    */
